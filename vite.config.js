44b0904b166cf928a20f1fc7fdf449753d14f232
Avinash Kumar <kravinash742@gmail.com>
2024-06-29 12:52:36 +0530

    Modify

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/FIND-WEATHER-APP/",
-  plugins: [react()],
+    plugins: [react()],
 })
